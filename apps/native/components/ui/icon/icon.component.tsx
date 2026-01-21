6909ddd6f2ca965c4c610b1e50f29e2b3a0f812b
Leonardo Melzer <leoimelzer@gmail.com>
2026-01-20 21:02:54 -0300

    Remove dependency on @react-navigation/native

--- apps/native/components/ui/icon/icon.component.tsx
+++ apps/native/components/ui/icon/icon.component.tsx
@@ -2,18 +2,19 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import { getStyles } from './icon.styles'
 import type { IconProps } from './icon.types'
-import { useTheme } from '@react-navigation/native'
+import { colors } from '@/constants'
+import { useColorScheme } from 'react-native'
 
 export function Icon(props: IconProps) {
-  const theme = useTheme()
-
   const styles = getStyles()
+
+  const colorScheme = useColorScheme() ?? 'light'
 
   return (
     <MaterialCommunityIcons
       {...props}
       size={props.size ?? 24}
-      color={props.color ?? theme.colors.text}
+      color={props.color ?? colors[colorScheme].text}
       style={[styles.container, props.style]}
     />
   )
