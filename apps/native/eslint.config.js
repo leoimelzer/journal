dfd8107f6f7de9114673c9caf363fc777466cf55
Leonardo Melzer <leoimelzer@gmail.com>
2026-01-20 21:02:09 -0300

    Configure eslint ignore

--- apps/native/eslint.config.js
+++ apps/native/eslint.config.js
@@ -5,7 +5,7 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['node_modules'],
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-require-imports': 'off',
