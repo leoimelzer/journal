e689861ac41d13a1c9aa3966e5c21acea8c18079
Leonardo Melzer <leoimelzer@gmail.com>
2026-01-24 20:23:05 -0300

    Update eslint rules

--- apps/native/eslint.config.js
+++ apps/native/eslint.config.js
@@ -1,18 +1,25 @@
 const expoConfig = require('eslint-config-expo/flat')
-const importHelpers = require('eslint-plugin-import-helpers')
-const prettier = require('eslint-plugin-prettier')
-const react = require('eslint-plugin-react')
-const reactNative = require('eslint-plugin-react-native')
+const importHelpersPlugin = require('eslint-plugin-import-helpers')
+const prettier_eslint = require('eslint-plugin-prettier')
+const react_eslint = require('eslint-plugin-react')
+const react_native_eslint = require('eslint-plugin-react-native')
+const typescript_eslint = require('@typescript-eslint/eslint-plugin')
+const typescript_parser = require('@typescript-eslint/parser')
 const { defineConfig } = require('eslint/config')
 
 module.exports = defineConfig([
   expoConfig,
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescript_parser
+    },
     plugins: {
-      prettier: prettier,
-      react: react,
-      'react-native': reactNative,
-      'import-helpers': importHelpers
+      prettier: prettier_eslint,
+      react: react_eslint,
+      'react-native': react_native_eslint,
+      'import-helpers': importHelpersPlugin,
+      '@typescript-eslint': typescript_eslint
     },
     rules: {
       'prettier/prettier': 'error',
@@ -20,6 +27,8 @@ module.exports = defineConfig([
       'react/prop-types': 'off',
       'react-native/no-unused-styles': 'error',
       'react-native/no-single-element-style-arrays': 'error',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
       'import-helpers/order-imports': [
         'warn',
         {
